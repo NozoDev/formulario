299f3a74b719945852150a5464f07ca452e7425f
Kevin rojas <kevinroj326@gmail.com>
2023-09-18 14:50:56 -0500

    una alerta agg para saber que el usuario se registro exitosamente

--- src/componets/Form.jsx
+++ src/componets/Form.jsx
@@ -1,49 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 const Form = () => {
+  const [formDate, setFormDate] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+    aceptoTerminos: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const newValue = type === "checkbox" ? checked : value;
+    setFormDate({ ...formDate, [name]: newValue });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setFormDate({
+      nombre: "",
+      email: "",
+      password: "",
+      aceptoTerminos: false,
+    });
+
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Your work has been saved",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
   return (
     <main className="main__form">
       <section className="section__form">
-        <form className="section__welcome">
+        <form className="section__welcome" onSubmit={handleSubmit}>
           <section className="section__contenedor">
             <section className="section__info">
               <h1 className="text">Welcome</h1>
               <p>Welcome to Dashboard</p>
               <a href="https://www.google.com/" target="_blank">
                 <i className="bx bxl-google-plus-circle">
-
                   Continue With Google
                 </i>
               </a>
 
               <p>-----or-----</p>
             </section>
+
+            <label className="label__description" htmlFor="Nombre">
+              Nombre
+            </label>
             <input
+              className="btn__input"
               type="text"
               id="nombre"
               name="nombre"
-              placeholder="Name:"
+              value={formDate.nombre}
+              onChange={handleChange}
               required
             />
             <br />
             <br />
-            <input type="email" id="email" placeholder="Email:" name="email" />
-            <br />
-            <br />
+            <label className="label__description" htmlFor="Nombre">
+              Email
+            </label>
             <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Password:"
+              className="btn__input"
+              type="email"
+              id="email"
+              name="email"
+              value={formDate.email}
+              onChange={handleChange}
             />
             <br />
             <br />
-            <div>
+            <label className="label__description" htmlFor="Nombre">
+              Password
+            </label>
+            <input
+              className="btn__input"
+              type="password"
+              name="password"
+              id="password"
+              value={formDate.password}
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+            <div className="check__box">
               <label htmlFor="aceptoTerminos">Acepto los terminos</label>
               <input
+                className="btn__input"
                 type="checkbox"
                 id="aceptoTerminos"
-                name="aceptoTerminos"
                 required
               />
             </div>
